9a6c60c886e9c828b6292d0bccf71decfa41770c
GauravSighariya <84697605+GauravSighariya@users.noreply.github.com>
2022-10-20 08:54:08 +0530

    Update settings.py

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+django_heroku.settings(locals(),staticfiles=False)
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
